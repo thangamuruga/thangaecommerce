68511a82ee690ea7e5a50eadffef121deb22faf6
Ubuntu <ubuntu@ip-172-31-17-33.ec2.internal>
2024-01-08 08:48:33 +0000

    changes

--- Myecommerce/thanga/admin.py
+++ Myecommerce/thanga/admin.py
@@ -1,14 +1,28 @@
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
-from . models import catagory
-from . models import product
+from .models import catagory, product
 
-class catagoryadmin(ImportExportModelAdmin,admin.ModelAdmin):
-	list_display=('catagoryid','catagoryname','status','created_at')
+class CatagoryResource(resources.ModelResource):
+    class Meta:
+        model = catagory
 
-class productadmin(ImportExportModelAdmin,admin.ModelAdmin):
-        list_display=('productid','productname','vendorname','quantity','status','created_at')
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = product
+        exclude = ('productid', 'productimage','created_at')
+        fields = ('catagoryname', 'productname', 'vendorname', 'pdescription', 'quantity', 'originalprice', 'sellingprice', 'status', 'Treanding' )
+        skip_unchanged = True
+        report_skipped = False
+
+class CatagoryAdmin(ImportExportModelAdmin):
+    list_display = ('catagoryid', 'catagoryname', 'status', 'created_at')
+    resource_class = CatagoryResource
+
+class ProductAdmin(ImportExportModelAdmin):
+    list_display = ('productid', 'productname', 'vendorname', 'quantity', 'status', 'created_at')
+    resource_class = ProductResource
 
 
-admin.site.register(catagory,catagoryadmin)
-admin.site.register(product,productadmin)
+admin.site.register(catagory, CatagoryAdmin)
+admin.site.register(product, ProductAdmin)
